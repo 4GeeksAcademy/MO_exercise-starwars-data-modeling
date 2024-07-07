ccd23f69bb1f8d1bbb2ae91d64cf452af74bd6e1
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-07-07 11:05:55 +0000

    finalizado

--- src/models.py
+++ src/models.py
@@ -32,53 +32,76 @@
 # render_er(Base, 'diagram.png')
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime # type: ignore
-from sqlalchemy.orm import relationship, declarative_base # type: ignore
-from sqlalchemy import create_engine # type: ignore
-from eralchemy2 import render_er # type: ignore
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
+
 
 Base = declarative_base()
 
-class Users(Base):
-    __tablename__ = 'users'
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    email = Column(String(50), nullable=False, unique=True)
-    first_name = Column(String(250), nullable=False)
-    last_name = Column(String(50))
-    password = Column(String(16), nullable=False)
+    name_user = Column(String(20))
+    apellidos_user = Column(String(40))
+    email_user = Column(String(50))
+    contraseña_user = Column(String(50))
 
-class Posts(Base):
-    __tablename__ = 'posts'
+class Planetas(Base):
+    __tablename__ = 'planetas'
     id = Column(Integer, primary_key=True)
-    title = Column(String(250), nullable=False)
-    description = Column(String(500))
-    body = Column(String(2500), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship(Users)
+    name_planeta = Column(String(20))
+    diameter_planeta = Column(Float)
+    gravity_planeta = Column(String(20))
+    surface_water_planeta = Column(Integer)
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    name_personaje = Column(String(20))
+    height_personaje = Column(Float)
+    mass_personaje = Column(Float)
+    gender_personaje = Column(String(10))
+    created_date_time = Column(DateTime)
+
+class Vehiculos(Base):
+    __tablename__ = 'vehiculos'
+    id = Column(Integer, primary_key=True)
+    name_vehiculo = Column(String(50))
+    model_vehiculo = Column(String(50))
+    passenger_vehiculo = Column(Integer)
+
+class PlanetasFavoritos(Base):
+    __tablename__ = 'planetas_favoritos'
+    id = Column(Integer, primary_key=True)
+    id_del_usuario = Column(Integer, ForeignKey('user.id')) #almacena el ID del usuario que ha marcado un planeta como favorito.
+    user = relationship(User) 
+    id_del_planeta = Column (Integer, ForeignKey('planetas.id')) #almacena el ID del planeta que ha sido marcado como favorito.
+    planetas = relationship(Planetas)
+
+class PersonajesFavoritos(Base):
+    __tablename__ = 'personajes_favoritos'
+    id = Column(Integer, primary_key=True)
+    id_del_usuario = Column(Integer, ForeignKey('user.id')) 
+    user = relationship(User) 
+    id_del_personaje = Column (Integer, ForeignKey('personajes.id')) #almacena el ID del personaje que ha sido marcado como favorito.
+    personajes = relationship(Personajes)
+
+class VehiculosFavoritos(Base):
+    __tablename__ = 'vehiculos_favoritos'
+    id = Column(Integer, primary_key=True)
+    id_del_usuario = Column(Integer, ForeignKey('user.id')) 
+    user = relationship(User) 
+    id_del_vehiculo = Column (Integer, ForeignKey('vehiculos.id')) #almacena el ID del vehiculo que ha sido marcado como favorito.
+    vehiculos = relationship(Vehiculos)
+
+
+
 
     def to_dict(self):
         return {}
-    
-class Characters(Base):
-    __tablename__ = 'characters'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    homeworld_id = Column(Integer, ForeignKey('planets.id'))
-    homeworld = relationship('Planets')
-
-class Planets(Base):
-    __tablename__ = 'planets'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-
-
-class Vehicles(Base):
-    __tablename__ = 'vehicles'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(150), nullable=False)
-    director = Column(String(150), nullable=False)
-    year = Column(DateTime)
-
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
